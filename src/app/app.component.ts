95ac8f04335a029ca2c1155a01cfd687400a3e00
7Lucas <7lucashp@gmail.com>
2022-10-25 19:17:42 -0300

    v1 -- titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Listar-e-Editar-Alunos';
+  title = 'Listar e Editar Alunos';
 }
